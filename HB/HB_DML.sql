bbac9e0ff290d1725dba126ce299892b90eb4ee7
PhucHoangVan <93416646+PhucHoangVan@users.noreply.github.com>
2021-11-06 19:06:38 +0700

    Upload Database Login, HB and KQHT

---
+++ HB/HB_DML.sql
@@ -0,0 +1,6 @@
+﻿-- DATA MANIPULATION LANGUAGE
+-- Thêm, xóa, sửa dữ liệu
+
+
+USE QLHT
+GO
---
+++ Login/Login_DML.sql
@@ -0,0 +1,18 @@
+﻿-- DATA MANIPULATION LANGUAGE
+-- Thêm, xóa, sửa dữ liệu
+
+
+USE QLHT
+GO
+
+INSERT INTO	SINHVIEN
+VALUES		
+			('20522122', N'Trường ĐHCNTT', 'PMCL2020.2', N'Kỹ thuật phần mềm', 2020, N'Bình Định'),
+			('20521760', N'Trường ĐHCNTT', 'PMCL2020.2', N'Kỹ thuật phần mềm', 2020, N'Thừa Thiên Huế')
+
+INSERT INTO ACCOUNT
+VALUES
+			('20522122', '1', N'Nguyễn Thái Tuấn', '20522122'),
+			('20521760', '1', N'Hoàng Văn Phúc', '20521760')
+
+
